63f50183d0567ef77aa072435f05084294d96d60
Charles Moss & Mike Chastain <student@epicodus.com>
2015-08-19 12:17:20 -0700

    add 3rd test_save and pass

--- tests/CuisinesTest.php
+++ tests/CuisinesTest.php
@@ -34,8 +34,35 @@
          $result = $test_cuisine->getStyle();
          //Assert
          $this->assertEquals($style, $result);
+     }//end test
+
+     function test_getId()
+     {
+         //Arrange
+         $style = "Thai";
+         $id = 1;
+         $test_cuisine = new Cuisine($style, $id);
+         //Act
+         $result = $test_cuisine->getId();
+         //Assert
+         $this->assertEquals(true, is_numeric($result));
      }
 
+     function test_save()
+     {
+         //arrange
+         $style = "Thai";
+         $test_cuisine = new Cuisine($style);
+
+         //act
+         $test_cuisine->save();
+
+         //assert
+         $result = Cuisine::getAll();
+         $this->assertEquals($test_cuisine,$result[0]);
+
+
+     }
 
       }
 
